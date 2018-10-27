a002ca3d783f01beb3eb4cf79d79c6a42d6ff034
shiroma_daisuke <shiroma_daisuke@m1005ndp1803.local>
2018-10-27 12:46:41 +0900

    Initial Commit

---
+++ conpass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  conpass
+//
+//  Created by shiroma_daisuke on 2018/10/27.
+//  Copyright © 2018年 shiroma_daisuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
