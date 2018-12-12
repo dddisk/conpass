fc899b7d4b6031cb6e1a203961c8c08d459265d8
shiroma_daisuke <shiroma_daisuke@M1005NDP1803.local>
2018-12-12 17:25:00 +0900

    SearchBar was displayed

--- conpass/AppDelegate.swift
+++ conpass/AppDelegate.swift
@@ -13,9 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var url: String?
+    var navigationController: UINavigationController?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let viewController: ViewController = ViewController()
+        
+        navigationController = UINavigationController(rootViewController: viewController)        
+        self.window?.rootViewController = navigationController
         return true
     }
 
--- conpass/ViewController.swift
+++ conpass/ViewController.swift
@@ -5,18 +5,14 @@ class ViewController: UIViewController{
     private var tableView = UITableView()
     private var mySystemButton: UIButton!
     var resultsfields: ConnpassViewModel = ConnpassViewModel(events: [])
-    var searchBar = UISearchBar()
-    
+    var leftBarButton: UIBarButtonItem!
+    var searchBar: UISearchBar!
 //  起動時にviewDidLoadが呼ばれ、中の処理を走らせる
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupSearchBar()
+        
         tableView.delegate = self
-        for i in 1...5 {
-            var sss:[Int] = []
-            sss.append(i)
-            print(sss)
-        }
         setUpTableView: do {
             tableView.frame = view.frame
             tableView.dataSource = self
@@ -33,10 +29,29 @@ class ViewController: UIViewController{
         })
 
     }
-    @objc func buttonEvent(_ sender: UIButton) {
-        print("ボタンの情報: \(sender)")
+    // ボタンをタップしたときのアクション
+    @objc func tappedLeftBarButton() {
+        let previousPage = PreviousViewController()
+        self.navigationController?.pushViewController(previousPage, animated: true)
+    }
+    
+}
+
+extension ViewController: UISearchBarDelegate {
+    func setupSearchBar() {
+        if let navigationBarFrame = navigationController?.navigationBar.bounds {
+            let searchBar: UISearchBar = UISearchBar(frame: navigationBarFrame)
+            searchBar.delegate = self as! UISearchBarDelegate
+            searchBar.placeholder = "タイトルで探す"
+            searchBar.tintColor = UIColor.gray
+            searchBar.keyboardType = UIKeyboardType.default
+            navigationItem.titleView = searchBar
+            navigationItem.titleView?.frame = searchBar.frame
+            self.searchBar = searchBar
+        }
     }
 }
+
 
 extension ViewController: UITableViewDataSource {
 
@@ -44,7 +59,7 @@ extension ViewController: UITableViewDataSource {
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
         let resultsfield = resultsfields.events[indexPath.row]
         cell.textLabel?.text = resultsfield.title
-        cell.detailTextLabel?.text = resultsfield.started_at
+        cell.detailTextLabel?.text = resultsfield.event_url
         return cell
     }
     
@@ -58,57 +73,23 @@ extension ViewController: UITableViewDataSource {
         print(ssss)
         ssss.sort(by: {$0.started_at < $1.started_at})
         print(ssss)
-        
+        for i in 1...5 {
+            var sss:[Int] = []
+            sss.append(i)
+            print(sss)
+        }
     }
     
-
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return resultsfields.events.count
     }
-
 }
 
 extension ViewController: UITableViewDelegate {
-
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let resultsfield = resultsfields.events[indexPath.row]
         let webPage = resultsfield.event_url
         let safariVC = SFSafariViewController(url: NSURL(string: webPage)! as URL)
         present(safariVC, animated: true, completion: nil)
     }
-    
-    
-}
-
-extension ViewController: UISearchBarDelegate {
-    //https://qiita.com/Simmon/items/8760de60162068781278 参考
-    //    http://blue-bear.jp/kb/swift4-tableview%E3%81%ABuisearchbar%E3%82%92%E5%AE%9F%E8%A3%85%E3%81%99%E3%82%8B/
-    func searchup(){
-        searchBar.delegate = self
-        searchBar.frame = CGRect(x:0, y:0, width:self.view.frame.width, height:42)
-        searchBar.layer.position = CGPoint(x: self.view.bounds.width/2, y: 89)
-        searchBar.searchBarStyle = UISearchBar.Style.default
-        searchBar.showsSearchResultsButton = false
-        searchBar.placeholder = "検索"
-        searchBar.setValue("キャンセル", forKey: "_cancelButtonText")
-        tableView.tableHeaderView = searchBar
-        print(searchBar.text)
-//        //searchBarの位置とサイズを設定
-//        searchBar.frame = CGRect(x:((self.view.bounds.width-320)/2),y:300,width:320,height:50)
-//        //薄文字の説明
-//        searchBar.placeholder = "ここに入力してください"
-//        //ViewにsearchBaroをSubViewとして追加
-//        self.view.addSubview(searchBar)
-    }
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        self.view.endEditing(true)
-        searchBar.showsCancelButton = true
-        self.tableView.reloadData()
-    }
-
-    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
-        searchBar.showsCancelButton = true
-        return true
-    }
-
 }
